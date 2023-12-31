392d53828882e2021a063d5d2675dcfcd7251cdf
Franco Galeano <francoxsg@gmail.com>
2023-12-31 02:16:20 -0300

    Se corrigen varios bugs y se crea HTML y CSS

--- main.js
+++ main.js
@@ -26,11 +26,17 @@ class Producto{
 const productosToString = (productos) => {
     let productosString="";
     for(const iterator of productos ){
-        productosString += "\n" + iterator.id + " - " + iterator.nombre + "  Descripcion: " + iterator.descripcion +" Precio: $" +iterator.precio;
+        productosString += "\n" + "ID: " + iterator.id + " - " + iterator.nombre + "  Descripcion: " + iterator.descripcion +" Precio: $" +iterator.precio;
     }
     return productosString;
 }
 
+const validarNumero = (entrada) =>{
+    while((entrada === "") || (isNaN(entrada))){
+        entrada = prompt("Indique un valor Numerico")
+    }
+    return entrada;
+}
 
 
 /* Declaraciones */
@@ -40,7 +46,7 @@ const tienda = {
     cantidadProductos(){return this.productos.length},
 
     buscador(palabra){
-        return this.productos.filter( (item) => item.nombre.includes(palabra));  //////////////////////////////////////////////////////////// Por que no Funciona?
+        return this.productos.filter( (item) => item.nombre.includes(palabra)); 
     }
 };
 
@@ -48,9 +54,6 @@ const carrito ={
     productos:[],
 
     total(){
-
-        /* let suma=this.productos.reduce((acum,item)=>{acum + item.precio, 0}); */ //////////////////////////////////////////////////////////// Por que no suma?
-        
         let suma=0;
         for(let j=0; j< this.productos.length;j++){
             suma += this.productos[j].precio;
@@ -62,7 +65,7 @@ const carrito ={
 /* Inicializacion de Tienda Predefinida */
 
 for(let i=0; i<3; i++){
-    const producto = new Producto({id:tienda.productos.length+1,nombre:"Campera " + (i+1), descripcion:"Campera de cuero talle M " + (i+1), precio:25000 + 100*i, vendedor:"Ashton Ton"});
+    const producto = new Producto({id:tienda.productos.length+1,nombre:"Campera " + (i+1), descripcion:"Campera talle M " + (i+1), precio:25000 + 100*i, vendedor:"Ashton Ton"});
     tienda.productos.push(producto);
 }
 
@@ -75,7 +78,9 @@ for(let i=0; i<3; i++){
 
 alert("¡Bienvenido a la Tienda Franquito!");
 
-while(true){
+let correrPrograma = true;
+
+while(correrPrograma){
     let operacion=0;
     operacion= Number(prompt(`Seleccione el numero de Operacion a Realizar:
     - Gestionar Tienda -
@@ -88,6 +93,8 @@ while(true){
     5 - Eliminar Producto del Carrito
     6 - Ver Carrito
     7 - Pagar
+
+    0 - Salir
     `));
 
 
@@ -104,20 +111,53 @@ while(true){
             Indique Nombre de Producto` ), 
             descripcion:prompt(`- NUEVO PRODUCTO -
             Indique Descripcion de Producto` ), 
-            precio:Number(prompt(`- NUEVO PRODUCTO -
-            Indique Precio de Producto` )), 
+            precio:validarNumero(Number(prompt(`- NUEVO PRODUCTO -
+            Indique Precio de Producto` ))), 
             vendedor:prompt(`- NUEVO PRODUCTO -
             Indique Vendedor de Producto` )});
         tienda.productos.push(producto);
             break;            
         case 3: /* Eliminar Producto de Tienda */
-
+            
+            let eliminarPorID =Number(prompt(`Seleccione el ID del producto a eliminar
+            
+            ${productosToString(tienda.productos)}`));
+            
+            let IDs = tienda.productos.map((item) => item.id); //Construyo array solo de IDs para usar indexOf  ///////////////////////////Esto se puede hacer de una mejor forma?
+            let indiceDeID = IDs.indexOf(eliminarPorID);       //Encoentro el index de ese ID
+            if(indiceDeID != -1){
+                tienda.productos.splice(indiceDeID,1);
+            }else{
+                alert("No se ha encontrado ese ID");
+            }
             break;            
-        case 4: /* Agregar al Carrito buscando Producto */
-
+        case 4: /* Agregar al Carrito buscando Producto */                                  
+            let resultadoBusqueda = tienda.buscador(prompt("Ingrese busqueda"));
+            let agregarID= Number(prompt(`Indique el ID para agregar al carrito, o "0" pasa salir
+            ${productosToString(resultadoBusqueda)}`));
+            if(agregarID != 0){
+                let IDsTienda = tienda.productos.map((item) => item.id);
+                let indiceAgregar = IDsTienda.indexOf(agregarID);
+                if(indiceAgregar != -1){
+                    carrito.productos.push(tienda.productos[indiceAgregar]);
+                }else{
+                    alert("No se ha encontrado ese ID");
+                    break;
+                }
+            }
             break;            
         case 5: /* Eliminar Producto del Carrito */
-
+        let eliminarPorIDCarrito =Number(prompt(`Seleccione el ID del producto a eliminar
+            
+        ${productosToString(carrito.productos)}`));
+        
+        let IDsCarrito = carrito.productos.map((item) => item.id); //Construyo array solo de IDs para usar indexOf  ///////////////////////////Esto se puede hacer de una mejor forma?
+        let indiceDeIDCarrito = IDsCarrito.indexOf(eliminarPorIDCarrito);       //Encoentro el index de ese ID
+        if(indiceDeIDCarrito != -1){
+            carrito.productos.splice(indiceDeIDCarrito,1);
+        }else{
+            alert("No se ha encontrado ese ID");
+        }
             break;            
         case 6: /* Ver Carrito */
         alert(`Este es el Carrito:
@@ -125,8 +165,13 @@ while(true){
         ${productosToString(carrito.productos)}`);
             break;            
         case 7: /* Pagar */
-
-            break;            
+        
+        let precios = carrito.productos.map((item) => item.precio); 
+        alert(`El total a pagar es de ${precios.reduce((acum, item)=> acum+item,0)}`)
+            break;   
+        case 0:
+            correrPrograma=false;
+            break;         
         default:
             alert("Opcion Invalida");
             break;            
